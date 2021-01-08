f184720283020399227c371697e97c9b522f99c3
guyfilo <guyfilo94@gmail.com>
2021-01-08 20:34:07 +0200

    declareNewVar

--- src/oop/ex6/main/LineParser.java
+++ src/oop/ex6/main/LineParser.java
@@ -19,22 +19,16 @@ public class LineParser {
     private final static Pattern METHOD_CALL_REGEX =
             Pattern.compile("^\\s*+(\\w++)\\s*+\\(([^\\)]*+)\\)\\s*+;\\s*+$");
     private final static String FINAL = "final";
-
     private final static String END_SCOPE_REGEX = "^[^}]*+}\\s*+$";
     private final static String DECLERATION_LINE = "^.*+;{1}$";
     private final static String NEW_SCOPE_REGEX = "^[^{]*+\\{\\s*+$";
     private final static Pattern CHANGE_VARIABLE_REGEX =
             Pattern.compile("^\\s*+\\w++\\s*+=\\s*+([^;\\s]*+)\\s*+;\\s*+$");
+    private final static String VAR_DECLARATION_REGEX =
+            "\\s++([^;]++)\\s*+;\\s*+$";
+    private final static Pattern UNINITIALISED_VAR = Pattern.compile("^\\s*+(\\w++)\\s*+$");
+    private final static Pattern INITIALISED_VAR = Pattern.compile("^\\s*+(\\w++)\\s*+=\\s*+(\\S++)\\s*+$");
 
-
-    public void statementLineParser(String line, Map<String, Variable> scopeVariables, Map<String, Variable> methods){
-    }
-
-    public void scopeBeginsParser(String line, Map<String, Variable> scopeVariables, Map<String, Variable> methods){
-    }
-
-    public void lineVerification(String line, Map<String, Variable> scopeVariables, Map<String, Variable> methods){
-    }
 
     public static boolean isReturnLine(String line){
         return line.matches(RETURN_LINE_REGEX);
@@ -53,10 +47,38 @@ public class LineParser {
         throw new GeneralException("");
     }
 
+    //todo: change to scope
     private static void declareNewVar
-            (String varLine, String type, boolean isFinal, Map<String, Variable> scopeVariables){
-
-
+            (String varLine, String type, boolean isFinal, Map<String, Variable> scopeVariables) throws GeneralException {
+            Matcher varDeclaration = Pattern.compile(type + VAR_DECLARATION_REGEX).matcher(varLine);
+            if (!varDeclaration.find()){
+                throw new GeneralException("declaration line must contain declarations");
+            }
+            String[] declarations = varDeclaration.group(1).split(",");
+            for (String declaration : declarations){
+                Matcher varMatcher = UNINITIALISED_VAR.matcher(declaration);
+                if (varMatcher.matches()){
+                    Variable newVar = VariableFactory.createNewVar
+                            (varMatcher.group(1), type, null, isFinal);
+                    scopeVariables.put(newVar.getName(), newVar);
+                    continue;
+                }
+                varMatcher.usePattern(INITIALISED_VAR);
+                if (varMatcher.matches()){
+                    Variable newVar;
+                    if (scopeVariables.containsKey(varMatcher.group(2))){
+                        Variable otherVar = scopeVariables.get(varMatcher.group(2));
+                        newVar = VariableFactory.createNewVar
+                                (otherVar, varMatcher.group(1), type, isFinal);
+                    } else {
+                        newVar = VariableFactory.createNewVar
+                                (varMatcher.group(1), type, varMatcher.group(2), isFinal);
+                    }
+                    scopeVariables.put(newVar.getName(), newVar);
+                    continue;
+                }
+                throw new GeneralException("bad declaration");
+            }
     }
 
     public static boolean varLineCheck(String varLine, Map<String, Variable> scopeVariables)
@@ -113,6 +135,11 @@ public class LineParser {
 
     public static boolean checkLoopLine(String line, Map<String, Variable> scopeVariables ){
         return false; //todo: write check
+    }
+
+
+    public static void main(String[] args) {
+
     }
 
 }
