d467f5b61ae11fb60ca6fac6350139b527f3d361
noaabarbanel <abarbanelnoa@gmail.com>
2021-01-08 16:41:00 +0200

    מעבר משותף

--- src/oop/ex6/scope/InnerScope.java
+++ src/oop/ex6/scope/InnerScope.java
@@ -1,4 +1,21 @@
 package oop.ex6.scope;
 
+import oop.ex6.jacasvariable.Variable;
+import oop.ex6.method.Method;
+
+import java.util.Map;
+
 public class InnerScope extends Scope {
+    //todo: for each method - inner scope
+
+    public InnerScope(String[] methodLines, Map<String,Variable> outerVariables, Map<String, Method> globalMethods, int scopeFirstLineIdx){
+        //todo: constractor
+
+    }
+
+    public int checkValidScope(){
+        //todo: check if method: line from kind ;,
+        //todo: if new scop - check id the first line is valid - if so - new inner scope
+        //todo: return the last scope idx line
+    }
 }
--- src/oop/ex6/scope/MainScope.java
+++ src/oop/ex6/scope/MainScope.java
@@ -1,13 +1,5 @@
 package oop.ex6.scope;
 
 public class MainScope extends Scope {
-    @Override
-    void createInnerScope() {
 
-    }
-
-    @Override
-    void createScope() {
-
-    }
 }
